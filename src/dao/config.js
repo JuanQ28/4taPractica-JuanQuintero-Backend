c03eae1ce9a1a35979f5e716c7a949bbb3f1086b
JuanQ28 <elquinteje@hotmail.com>
2023-10-29 21:22:30 -0500

    implementación de .env

--- src/dao/config.js
+++ src/dao/config.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
+import { config } from "dotenv";
 
-const URI = "mongodb+srv://elquinteje:<password>@cluster0.fy8hs8n.mongodb.net/ecommerce?retryWrites=true&w=majority"
+const dotenv = config()
+
+const URI = `mongodb+srv://elquinteje:${process.env.URI_PASSWORD}@cluster0.fy8hs8n.mongodb.net/ecommerce?retryWrites=true&w=majority`
+
 
 mongoose
     .connect(URI)
